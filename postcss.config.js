244b03ecd3da06d3714623cbe0b577ab4fd1cdc2
candland <candland@gmail.com>
2019-07-31 17:41:21 -0600

    update postcss config.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,24 @@
-module.exports = {
+const config = {
   plugins: {
     'tailwindcss': 'tailwind.config.js',
-    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production',
     'autoprefixer': {}
   }
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins['@fullhuman/postcss-purgecss'] = {
+    content: ['./src/**/*.html', './src/**/*.svg', './src/**/*.js', './src/**/*.njk'],
+    extractors: [
+      {
+        extensions: ['html', 'svg', 'js', 'njk'],
+        extractor: class TailwindExtractor {
+          static extract (content) {
+            return content.match(/[A-Za-z0-9-_:/]+/g) || []
+          }
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config

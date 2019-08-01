81b84ee770655e1ebea1d45c1480a8aff3a20796
candland <candland@gmail.com>
2019-08-01 13:37:07 -0600

    add nav tag & nav to header. use default eleventyExcludeFromCollections.

--- .eleventy.js
+++ .eleventy.js
@@ -4,31 +4,59 @@ module.exports = function (eleventyConfig) {
   // Add a filter using the Config API
   /* eleventyConfig.addFilter( "myFilter", function() {}); */
 
-  eleventyConfig.addNunjucksFilter('keys', function (value) {
+  eleventyConfig.addFilter('keys', function (value) {
     return Object.keys(value)
   })
 
-  eleventyConfig.addNunjucksFilter('isoDate', function (date) {
+  eleventyConfig.addFilter('isoDate', function (date) {
     return date.toISOString()
   })
 
-  eleventyConfig.addNunjucksFilter('prepend', function (value, pre) {
+  eleventyConfig.addFilter('prepend', function (value, pre) {
     return `${pre}${value}`
   })
 
-  eleventyConfig.addNunjucksFilter('firstValue', function (values) {
+  eleventyConfig.addFilter('firstValue', function (values) {
     return values.find((val) => {
       return val !== null && val !== '' && val !== undefined
     })
   })
 
-	eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
+  eleventyConfig.addFilter('ordered', function (coll) {
+    return coll.sort((a, b) => {
+      return b.data.order - a.data.order
+    })
+  })
+
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
+      return array.slice(n)
     }
 
-    return array.slice(0, n);
-  });
+    return array.slice(0, n)
+  })
+
+  eleventyConfig.addFilter('prevPost', function (current) {
+    const items = this.ctx.collections.post
+    let index = items.findIndex((el) => {
+      return el.url === current.url
+    })
+    if (index === 0) {
+      index = items.length
+    }
+    return items[index - 1]
+  })
+
+  eleventyConfig.addFilter('nextPost', function (current) {
+    const items = this.ctx.collections.post
+    let index = items.findIndex((el) => {
+      return el.url === current.url
+    }) + 1
+    if (index === (items.length)) {
+      index = 0
+    }
+    return items[index]
+  })
 
   eleventyConfig.addPlugin(pluginRss)
 
